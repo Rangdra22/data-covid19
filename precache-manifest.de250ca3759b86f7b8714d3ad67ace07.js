269c04d3d1c623a58ac6a17fa338471f7ec9dd8a
Rangdra22 <63014773+Rangdra22@users.noreply.github.com>
2020-09-02 20:11:34 +0700

    Updates

---
+++ precache-manifest.de250ca3759b86f7b8714d3ad67ace07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feda4d2278498fb58493e575c1380b93",
+    "url": "/data-covid19/index.html"
+  },
+  {
+    "revision": "4aa9ca920c08e1e72b9a",
+    "url": "/data-covid19/static/css/main.78772868.chunk.css"
+  },
+  {
+    "revision": "b985755db3b545e5a03e",
+    "url": "/data-covid19/static/js/2.48d5f8ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/data-covid19/static/js/2.48d5f8ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa9ca920c08e1e72b9a",
+    "url": "/data-covid19/static/js/main.3066f834.chunk.js"
+  },
+  {
+    "revision": "fc2b5683233122fd6f9a",
+    "url": "/data-covid19/static/js/runtime-main.a6d85e73.js"
+  }
+]);
